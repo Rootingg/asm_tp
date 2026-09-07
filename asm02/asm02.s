330ea9720ab0aa7693b665c6f9c5eed98e16fe01
Rootingg <91620858+Rootingg@users.noreply.github.com>
2026-09-07 11:19:58 +0200

    fix asm02

--- asm02/asm02.s
+++ asm02/asm02.s
@@ -1,32 +1,35 @@
 section .data
-    message db "1337",4
-    input db 0
+    message db "1337", 13, 10, 0   
+    
+section .bss
+    input resb 5                   
 
 section .text
 global _start
 
 _start:
-
-    mov rax,0
-    mov rdi,0
-    mov rsi,input
-    mov rdx,4
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, input
+    mov rdx, 5
     syscall
 
-    cmp word [input], "42"
+    cmp word [input], 0x3234     
+    jne exit
+    cmp byte [input+2], 10         
     jne exit
 
-    mov rax,1
-    mov rdi,1
-    mov rsi,message
-    mov rdx,4
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, message
+    mov rdx, 6                      
     syscall
 
-    mov rax,60
-    xor rdi,rdi
+    mov rax, 60
+    xor rdi, rdi
     syscall
 
 exit:
-    mov rax,60
-    mov rdi,1
+    mov rax, 60
+    mov rdi, 1
     syscall
